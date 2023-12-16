fc7fe4e17cffec30e4343096fa915fbc3d4cba89
jacky <softwarecheng@gmail.com>
2023-12-16 14:30:41 +0800

    fix

--- bitcoin/main/service.sh
+++ bitcoin/main/service.sh
@@ -2,7 +2,7 @@
 
 script_path=$(cd "$(dirname "$0")" && pwd)
 node1Path="$script_path"
-node1DataPath=/media/sf_bitcoin/testnet
+node1DataPath=/media/sf_bitcoin/main
 node1CookiePath="$node1DataPath/.cookie"
 if [ ! -d "$node1DataPath" ]; then
     mkdir -p "$node1DataPath"
