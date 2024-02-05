a05d6e6ba4926ee24fcfdb1fd46112d50429cfd0
jacky <softwarecheng@126.com>
2024-02-05 10:20:05 +0800

    fix

---
+++ monitor/monitor-check-postgresql-mainnet.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+
+while true; do
+    if ! nc -z localhost 5432; then
+        echo "PostgreSQL server is not responding, attempting to restart..."
+        systemctl restart postgresql
+    else
+        result=$(psql -h localhost -d ord2_mainnet -U postgres -c "SELECT * from ord2_event_types;" 2>&1)
+        if [ $? -ne 0 ]; then
+            echo "Failed to execute psql command: $result"
+        else
+            echo "psql command executed successfully:"
+            echo "$result"
+        fi
+    fi
+    sleep 10
+done
