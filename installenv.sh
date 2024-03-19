1cf4f08c7a9f7a674027b3849444adf0cd0e48a4
jacky <softwarecheng@126.com>
2024-03-19 21:48:57 +0800

    fix

--- installenv.sh
+++ installenv.sh
@@ -67,3 +67,20 @@ apt install autossh
 
 # golang
 curl -sSL https://git.io/g-install | sh -s
+
+# backup linux
+# https://askubuntu.com/questions/7809/how-to-back-up-my-entire-system
+sudo tar cvpzf /data2/system-backup.tar.gz \
+--exclude=/run \
+--exclude=/dev \
+--exclude=/mnt \
+--exclude=/proc \
+--exclude=/sys \
+--exclude=/tmp \
+--exclude=/media \
+--exclude=/cdrom \
+--exclude=/data1 \
+--exclude=/data2 \
+--exclude=/lost+found \
+/
+tar xvpfz /data2/system-backup.tar.gz -C /
