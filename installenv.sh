84dda6c16c18244e8d5554e3f1d6b66c5d42d0dd
jacky <softwarecheng@126.com>
2024-03-19 18:46:27 +0800

    fix

--- installenv.sh
+++ installenv.sh
@@ -45,6 +45,7 @@ apt install postgresql postgresql-contrib
 
 # tmux
 apt remove tmux
+sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev
 sudo apt update && sudo apt install libncurses5-dev libevent-dev build-essential autoconf bison
 git clone git@github.com:tmux/tmux.git
 cd tmux && git checkout 3.3
