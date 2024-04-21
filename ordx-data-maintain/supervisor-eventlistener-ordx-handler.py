a77b476e4a0523ed881e87fa54798b4ce0d2dc06
jacky <softwarecheng@126.com>
2024-04-22 03:04:45 +0800

    fix

--- ordx-data-maintain/supervisor-eventlistener-ordx-handler.py
+++ ordx-data-maintain/supervisor-eventlistener-ordx-handler.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from datetime import datetime
+import shlex
 import sys
 import subprocess  # Added for calling an external bash script
 
@@ -12,15 +14,13 @@ def write_stderr(s):
 
 def run_command(command):
     try:
-        result = subprocess.Popen(
+        result = subprocess.run(
             command,
-            stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
-            # shell=True
+            text=True  # This makes the output to be str, not bytes
         )
-        stdout, stderr = result.communicate()
-        write_stderr('STDOUT: {}\n'.format(stdout.decode()))
-        write_stderr('STDERR: {}\n'.format(stderr.decode()))
+        if result.stderr:
+            write_stderr('STDERR: {}\n'.format(result.stderr))
     except Exception as e:
         write_stderr('An error occurred: {}\n'.format(e))
         
@@ -28,13 +28,15 @@ def main():
     while True:
         write_stdout('READY\n')
         line = sys.stdin.readline()
+        write_stderr("line: " + line + "\n")
         headers = dict([x.split(':') for x in line.split()])
         data = sys.stdin.read(int(headers['len']))
         payload = dict([x.split(':') for x in data.split()])
         if payload.get('expected', '0') == '0':
             processname = payload.get('processname')     
-            write_stderr("processname: " + processname)
+            write_stderr("processname: " + processname + "\n")
             if processname == 'ordx-server-master-testnet':
+                write_stderr("run command b2r.sh" + "\n")
                 run_command([
                     "/root/ordx-data-maintain/b2r.sh",
                     "-m", "recover",
@@ -44,8 +46,12 @@ def main():
                     "-b", "/data2/ordxData-backup",
                     "-o", "latest"
                 ])
+                write_stderr("run command supervisorctl\n")
                 run_command(['supervisorctl', 'start', 'ordx-server-master-testnet'])
+                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                write_stderr("processname resume data and program is success, current_time: " + current_time + "\n")
             elif processname == 'ordx-server-master-main':
+                write_stderr("run command b2r.sh" + "\n")
                 run_command([
                     "/root/ordx-data-maintain/b2r.sh",
                     "-m", "recover",
@@ -55,7 +61,10 @@ def main():
                     "-b", "/data2/ordxData-backup",
                     "-o", "latest"
                 ])
+                write_stderr("run command supervisorctl\n")
                 run_command(['supervisorctl', 'start', 'ordx-server-master-main'])
+                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                write_stderr("processname resume data and program is success, current_time: " + current_time + "\n")
         write_stdout('RESULT 2\nOK')
 
 if __name__ == '__main__':
