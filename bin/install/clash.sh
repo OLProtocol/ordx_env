d31b460d9936ebe65c619e30cb4e232effd02912
jacky <softwarecheng@126.com>
2024-04-30 14:48:55 +0800

    fix gc

--- bin/install/clash.sh
+++ bin/install/clash.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # set -x
 set -e
-git submodule update --init --recursive
+
+script_path=$(cd "$(dirname "$0")" && pwd)
+projectPath="$script_path/../.."
+cd "$projectPath"
+git submodule update --init --recursive clash
