df6fbd7ca4c23676151253de0c14d9ee669b7868
jacky <softwarecheng@126.com>
2024-04-30 16:10:53 +0800

    fix gc

--- bin/install/rust.sh
+++ bin/install/rust.sh
@@ -12,10 +12,10 @@ then
 else
     echo "preparing to install rustc"
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    # shellcheck disable=SC1091
+    source "$HOME/.cargo/env"
     rustc --version
     rustup toolchain install 1.70
     rustup default 1.70
-    # shellcheck disable=SC1091
-    source "$HOME/.cargo/env"
     # rustup show, rustup toolchain list
 fi
