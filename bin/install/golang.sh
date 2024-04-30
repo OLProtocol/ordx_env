b0c0f069f4a4746076d3f4f700babb71d5970e8b
jacky <softwarecheng@126.com>
2024-04-30 15:28:59 +0800

    fix gc

--- bin/install/golang.sh
+++ bin/install/golang.sh
@@ -2,5 +2,15 @@
 # set -x
 set -e
 
-# golang
+# golang,
+echo "must input alias name with gvm"
 curl -sSL https://git.io/g-install | sh -s
+gvm install 1.22.1
+gvm set 1.22.1
+go install -v golang.org/x/tools/gopls@latest
+go install -v github.com/cweill/gotests/gotests@latest
+go install -v github.com/fatih/gomodifytags@latest
+go install -v github.com/josharian/impl@latest
+go install -v github.com/haya14busa/goplay/cmd/goplay@latest
+go install -v github.com/go-delve/delve/cmd/dlv@latest
+go install -v honnef.co/go/tools/cmd/staticcheck@latest
