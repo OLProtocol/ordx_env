3e5a5c69a5291b32a91c2c011b1be24e51469fea
jacky <softwarecheng@126.com>
2024-06-27 16:57:52 +0800

    fix

--- bin/backup/system.sh
+++ bin/backup/system.sh
@@ -5,16 +5,17 @@ set -e
 # backup linux
 # https://askubuntu.com/questions/7809/how-to-back-up-my-entire-system
 sudo tar cvpzf /data2/system-backup.tar.gz \
---exclude=/run \
---exclude=/dev \
---exclude=/mnt \
---exclude=/proc \
---exclude=/sys \
---exclude=/tmp \
---exclude=/media \
---exclude=/cdrom \
---exclude=/data1 \
---exclude=/data2 \
---exclude=/lost+found \
-/
+    --exclude=/run \
+    --exclude=/dev \
+    --exclude=/mnt \
+    --exclude=/proc \
+    --exclude=/sys \
+    --exclude=/tmp \
+    --exclude=/media \
+    --exclude=/cdrom \
+    --exclude=/data1 \
+    --exclude=/data2 \
+    --exclude=/data \
+    --exclude=/lost+found \
+    /
 tar xvpfz /data2/system-backup.tar.gz -C /
