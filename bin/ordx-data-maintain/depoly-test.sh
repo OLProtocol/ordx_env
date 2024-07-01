3e2e2d238a47a463f185ce14a353baab2c2db2b4
jacky <softwarecheng@126.com>
2024-07-01 10:34:36 +0800

    fix

--- bin/ordx-data-maintain/depoly-test.sh
+++ bin/ordx-data-maintain/depoly-test.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-# backup latest ordx data for test
+# backup latest mainnet ordx data for test
 rm -rf /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2
 scp -r root@192.168.1.103:/data2/ordx-data-backup/mainnet/mainnet-849000-1.1.2 /data/ordx-data-backup/mainnet/
 
-# resume latest ordx data for test
+# resume latest mainnet ordx data for test
 supervisorctl stop ordx-mainnet
 rm -rf /data/ordx-data/mainnet/*
 cpg -gR /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2/* /data/ordx-data/mainnet/
+cd /data/github/ordx && go build -o ordx-mainnet
 supervisorctl start ordx-mainnet
 
-# backup latest ordx for prd
+# backup latest mainnet ordx for prd
 ssh root@192.168.1.101 rm -rf /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2
 scp -r /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2/* root@192.168.1.101:/data/ordx-data-backup/mainnet/
 
-# resume latest ordx data for prd master
+# resume latest mainnet ordx data for prd master
 ssh root@192.168.1.101 supervisorctl stop ordx-mainnet-master
 ssh root@192.168.1.101 rm -rf /data/ordx-data/mainnet-master/*
 ssh root@192.168.1.101 cpg -gR /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2/* root@192.168.1.101:/data/ordx-data/mainnet-master/
+scp /data/github/ordx/ordx-mainnet root@192.168.1.101:/usr/bin/rdx-testnet4-master
 ssh root@192.168.1.101 supervisorctl start ordx-mainnet-master
 
-# resume latest ordx data for test slave
+# resume latest mainnet ordx data for test slave
 ssh root@192.168.1.101 supervisorctl stop ordx-mainnet-slave
 ssh root@192.168.1.101 rm -rf /data/ordx-data/mainnet-slave/*
 ssh root@192.168.1.101 cpg -gR /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2/* root@192.168.1.101:/data/ordx-data/mainnet-slave/
+scp /data/github/ordx/ordx-mainnet root@192.168.1.101:/usr/bin/rdx-testnet4-slave
 ssh root@192.168.1.101 supervisorctl start ordx-mainnet-slave
