d39ae254be48d75e6e1f94b95de1eaa9f0e4316f
jacky <softwarecheng@126.com>
2024-07-01 13:14:49 +0800

    fix

--- bin/ordx-data-maintain/depoly-testnet4.sh
+++ bin/ordx-data-maintain/depoly-testnet4.sh
@@ -9,11 +9,11 @@ supervisorctl start ordx-testnet4
 # reset latest testnet4 ordx for prd master
 ssh root@192.168.1.101 supervisorctl stop ordx-testnet4-master
 ssh root@192.168.1.101 rm -rf /data/ordx-data/testnet4-master/*
-scp /data/github/ordx/ordx-testnet root@192.168.1.101:/usr/bin/ordx-testnet4-master
+scp /data/github/ordx-testnet/ordx-testnet root@192.168.1.101:/usr/bin/ordx-testnet4-master
 ssh root@192.168.1.101 supervisorctl start ordx-testnet4-master
 
 # resume latest testnet4 ordx for prd slave
 ssh root@192.168.1.101 supervisorctl stop ordx-testnet4-slave
 ssh root@192.168.1.101 rm -rf /data/ordx-data/testnet4-slave/*
-scp /data/github/ordx/ordx-testnet root@192.168.1.101:/usr/bin/ordx-testnet4-slave
+scp /data/github/ordx-testnet/ordx-testnet root@192.168.1.101:/usr/bin/ordx-testnet4-slave
 ssh root@192.168.1.101 supervisorctl start ordx-testnet4-slave
