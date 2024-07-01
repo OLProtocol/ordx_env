76c6832726b968f20b6c2c02bdebeffdd4307667
jacky <softwarecheng@126.com>
2024-07-01 11:08:49 +0800

    fix

--- bin/ordx-data-maintain/depoly-mainnet.sh
+++ bin/ordx-data-maintain/depoly-mainnet.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# backup latest mainnet ordx data for test
+# backup latest mainnet ordx for test
 rm -rf /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2
 scp -r root@192.168.1.103:/data2/ordx-data-backup/mainnet/mainnet-849000-1.1.2 /data/ordx-data-backup/mainnet/
 
-# resume latest mainnet ordx data for test
+# resume latest mainnet ordx for test
 supervisorctl stop ordx-mainnet
 rm -rf /data/ordx-data/mainnet/*
 cpg -gR /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2/* /data/ordx-data/mainnet/
@@ -12,18 +12,18 @@ supervisorctl start ordx-mainnet
 
 # backup latest mainnet ordx for prd
 ssh root@192.168.1.101 rm -rf /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2
-scp -r /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2/* root@192.168.1.101:/data/ordx-data-backup/mainnet/
+scp -r /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2 root@192.168.1.101:/data/ordx-data-backup/mainnet/
 
-# resume latest mainnet ordx data for prd master
+# resume latest mainnet ordx for prd master
 ssh root@192.168.1.101 supervisorctl stop ordx-mainnet-master
 ssh root@192.168.1.101 rm -rf /data/ordx-data/mainnet-master/*
-ssh root@192.168.1.101 cpg -gR /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2/* root@192.168.1.101:/data/ordx-data/mainnet-master/
-scp /data/github/ordx/ordx-mainnet root@192.168.1.101:/usr/bin/rdx-testnet4-master
+ssh root@192.168.1.101 cpg -gR /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2/* /data/ordx-data/mainnet-master/
+scp /data/github/ordx/ordx-mainnet root@192.168.1.101:/usr/bin/ordx-mainnet-master
 ssh root@192.168.1.101 supervisorctl start ordx-mainnet-master
 
-# resume latest mainnet ordx data for test slave
+# resume latest mainnet ordx for prd slave
 ssh root@192.168.1.101 supervisorctl stop ordx-mainnet-slave
 ssh root@192.168.1.101 rm -rf /data/ordx-data/mainnet-slave/*
-ssh root@192.168.1.101 cpg -gR /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2/* root@192.168.1.101:/data/ordx-data/mainnet-slave/
-scp /data/github/ordx/ordx-mainnet root@192.168.1.101:/usr/bin/rdx-testnet4-slave
+ssh root@192.168.1.101 cpg -gR /data/ordx-data-backup/mainnet/mainnet-849000-1.1.2/* /data/ordx-data/mainnet-slave/
+scp /data/github/ordx/ordx-mainnet root@192.168.1.101:/usr/bin/ordx-mainnet-slave
 ssh root@192.168.1.101 supervisorctl start ordx-mainnet-slave
---
+++ bin/ordx-data-maintain/depoly-testnet4.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# reset testnet4 ordx for test
+supervisorctl stop ordx-testnet4
+rm -rf /data/ordx-data/testnet4/*
+cd /data/github/ordx-testnet && git pull && go build -o ordx-testnet
+supervisorctl start ordx-testnet4
+
+# reset latest testnet4 ordx for prd master
+ssh root@192.168.1.101 supervisorctl stop ordx-testnet4-master
+ssh root@192.168.1.101 rm -rf /data/ordx-data/testnet4-master/*
+scp /data/github/ordx/ordx-testnet root@192.168.1.101:/usr/bin/ordx-testnet4-master
+ssh root@192.168.1.101 supervisorctl start ordx-testnet4-master
+
+# resume latest testnet4 ordx for prd slave
+ssh root@192.168.1.101 supervisorctl stop ordx-testnet4-slave
+ssh root@192.168.1.101 rm -rf /data/ordx-data/testnet4-slave/*
+scp /data/github/ordx/ordx-testnet root@192.168.1.101:/usr/bin/ordx-testnet4-slave
+ssh root@192.168.1.101 supervisorctl start ordx-testnet4-slave
