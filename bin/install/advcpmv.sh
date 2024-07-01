0d7ab8e3a80c2c6920137c92088f47890cbda435
jacky <softwarecheng@126.com>
2024-07-01 10:20:47 +0800

    fix

---
+++ bin/install/advcpmv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# https://github.com/jarun/advcpmv
+export FORCE_UNSAFE_CONFIGURE=1
+curl https://raw.githubusercontent.com/jarun/advcpmv/master/install.sh --create-dirs -o ./advcpmv/install.sh && (cd advcpmv && sh install.sh)
+cd advcpmv || exit
+cp advcp /usr/local/bin/gcp
+cp advmv /usr/local/bin/gmv
