9c4ffd6f0df1bd2e4e6eaea98ea5e7fdfed8ff8c
jacky <softwarecheng@126.com>
2024-07-03 17:13:18 +0800

    fix

--- bin/depoly/test/depoly-data-testnet4.sh
+++ bin/depoly/test/depoly-data-testnet4.sh
@@ -10,13 +10,15 @@ supervisorctl start ordx-testnet4
 # testnet4 ordx for prd master
 echo "deploying(reset)  testnet4 ordx data on 192.168.1.101 for prd master"
 ssh root@192.168.1.101 supervisorctl stop ordx-testnet4-master
-ssh root@192.168.1.101 rm -rf /data/ordx-data/testnet4-master/*
+ssh root@192.168.1.101 rm -rf /data/ordx-data/testnet4-master
+ssh root@192.168.1.101 mkdir -p /data/ordx-data/testnet4-master
 scp /data/github/ordx-testnet/ordx-testnet root@192.168.1.101:/usr/bin/ordx-testnet4-master
 ssh root@192.168.1.101 supervisorctl start ordx-testnet4-master
 
 # testnet4 ordx for prd slave
 echo "deploying(reset) testnet4 ordx data on 192.168.1.101 for prd slave"
 ssh root@192.168.1.101 supervisorctl stop ordx-testnet4-slave
-ssh root@192.168.1.101 rm -rf /data/ordx-data/testnet4-slave/*
+ssh root@192.168.1.101 rm -rf /data/ordx-data/testnet4-slave
+ssh root@192.168.1.101 mkdir -p /data/ordx-data/testnet4-slave
 scp /data/github/ordx-testnet/ordx-testnet root@192.168.1.101:/usr/bin/ordx-testnet4-slave
 ssh root@192.168.1.101 supervisorctl start ordx-testnet4-slave
