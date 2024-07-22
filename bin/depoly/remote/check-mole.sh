f18c72a8327aeb09be5ab2fd0a948f7d39a61f60
softwarecheng <softwarecheng@126.com>
2024-07-22 20:18:06 +0800

    fix

--- bin/depoly/remote/check-mole.sh
+++ bin/depoly/remote/check-mole.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# 定义要检查的端口
 ports=(8001 8007 8003 8008 8005 8009)
 
-# 检查端口函数
 check_port() {
     port=$1
     result=$(lsof -i:"$port" | wc -l)
@@ -11,15 +9,13 @@ check_port() {
         echo "Port $port is not running. Restarting sshd..."
         systemctl restart sshd
     else
-        # 检查超过1行的情况
         lsof -i:"$port" | awk 'NR > 1 && $8 ~ /TIME_WAIT|CLOSE_WAIT/ && ++count[$8] > 3 {system("kill -9 " $2)}'
     fi
 }
 
-# 循环检查端口
 while true; do
     for port in "${ports[@]}"; do
         check_port "$port"
     done
-    sleep 300 # 等待5分钟
+    sleep 300
 done
