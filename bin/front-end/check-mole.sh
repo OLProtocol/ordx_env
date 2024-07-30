fb1457d47c3ce02fcf8a80d8dd67ddc3b0f1e52f
softwarecheng <softwarecheng@126.com>
2024-07-30 16:35:23 +0800

    fix

---
+++ bin/front-end/check-mole.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# set -x
+set -e
+
+# Define the ports to check
+ports=(8001 8007 8003 8008 8005 8009)
+
+# Function to check a port
+check_port() {
+    port="$1"
+    echo "Start checking port $port"
+    result=$(lsof -i:"$port" | wc -l)
+    if [ "$result" -lt 2 ]; then
+        systemctl restart sshd
+        echo "sshd service for port $port is not running. Restarting sshd..."
+    else
+        # Check for more than one line
+        # lsof -i:"$port" | awk 'NR > 1 && $8 ~ /TIME_WAIT|CLOSE_WAIT/ && ++count[$8] > 3 {system("kill -9 " $2)}'
+        # lsof -i:"$port" | awk 'NR > 1 && $8 ~ /TIME_WAIT|CLOSE_WAIT/ {system("kill -9 " $2)}'
+        lsof -i:"$port" | awk 'NR > 1 && $8 ~ /TIME_WAIT/ {system("kill -9 " $2)}'
+        lsof -i:"$port" | awk 'NR > 1 && $8 ~ /CLOSE_WAIT/ {system("kill -9 " $2)}'
+        echo "check port $port for TIME_WAIT and CLOSE_WAIT and kill it"
+    fi
+}
+
+# Loop to check ports
+while true; do
+    echo "Start checking ports..."
+    for port in "${ports[@]}"; do
+        check_port "$port"
+    done
+    echo "Checking ports is done, waiting for 5 minutes..."
+    sleep 300 # Wait for 5 minutes
+done
