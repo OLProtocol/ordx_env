c2c780200b08ce3f7c8510fff0fc60e826562cfa
softwarecheng <softwarecheng@126.com>
2024-08-17 22:15:50 +0800

    fix btcd and testnet4 help

--- help/btcd/gen-addr.sh
+++ help/btcd/gen-addr.sh
@@ -3,9 +3,8 @@
 RPC_USER="jacky"
 RPC_PASS="123456"
 RPC_HOST="192.168.10.102"
-RPC_PORT="18334"
+
 TLS_CERT_PATH="./rpc.cert"
-NEW_ADDRESS=$(btcctl --regtest --rpcuser=$RPC_USER --rpcpass=$RPC_PASS --rpcserver=$RPC_HOST --rpcport=$RPC_PORT --rpccert=$TLS_CERT_PATH getnewaddress)
-btcctl --regtest --rpcuser=jacky--rpcpass=123456 --rpcserver=192.168.10.102 --rpcport=18334 --rpccert=./rpc.cert getnewaddress
+NEW_ADDRESS=$(btcctl -C ./regtest.conf --regtest --rpcuser=$RPC_USER --rpcpass=$RPC_PASS --rpcserver=$RPC_HOST --rpccert=$TLS_CERT_PATH getnewaddress)
 
 echo "New Bitcoin address: $NEW_ADDRESS"
--- help/btcd/mint.sh
+++ help/btcd/mint.sh
@@ -5,16 +5,15 @@ FIXED_ADDRESS=$1
 RPC_USER="jacky"
 RPC_PASS="123456"
 RPC_HOST="192.168.10.102"
-RPC_PORT="18334"
 TLS_CERT_PATH="./rpc.cert"
 
 CHECK_INTERVAL=30
 
 while true; do
-  TX_COUNT=$(btcctl --regtest --rpcuser=$RPC_USER --rpcpass=$RPC_PASS --rpcserver=$RPC_HOST --rpcport=$RPC_PORT --rpccert=$TLS_CERT_PATH getrawmempool | jq '. | length')
+  TX_COUNT=$(btcctl -C ./regtest.conf --regtest --rpcuser=$RPC_USER --rpcpass=$RPC_PASS --rpcserver=$RPC_HOST --rpccert=$TLS_CERT_PATH getrawmempool | jq '. | length')
   if [ "$TX_COUNT" -gt 0 ]; then
     echo "New transactions found in mempool: $TX_COUNT. Mining a new block..."
-    btcctl --regtest --rpcuser=$RPC_USER --rpcpass=$RPC_PASS --rpcserver=$RPC_HOST --rpcport=$RPC_PORT --rpccert=$TLS_CERT_PATH generate 1 "$FIXED_ADDRESS"
+    btcctl -C ./regtest.conf --regtest --rpcuser=$RPC_USER --rpcpass=$RPC_PASS --rpcserver=$RPC_HOST --rpccert=$TLS_CERT_PATH generate 1 "$FIXED_ADDRESS"
   else
     echo "No new transactions in mempool."
   fi
