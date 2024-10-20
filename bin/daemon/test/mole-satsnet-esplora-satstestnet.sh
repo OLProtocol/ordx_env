4cd8ab7e93f460676d8ee5447851be4b8d2c54c4
softwarecheng <softwarecheng@126.com>
2024-10-20 12:54:13 +0800

    update bin

--- bin/daemon/test/mole-satsnet-esplora-satstestnet.sh
+++ bin/daemon/test/mole-satsnet-esplora-satstestnet.sh
@@ -2,17 +2,17 @@
 
 set -x
 while true; do
-    if ! pgrep -f "mole start remote --verbose --source 103.103.245.177:3004" >/dev/null; then
+    if ! pgrep -f "mole start remote --verbose --source 103.103.245.177:5000" >/dev/null; then
         echo "Starting new process..."
         script_dir=$(dirname "$(readlink -f "$0")")
         shfilename=$(basename -- "$0")
         shfilename="${shfilename%.*}"
         logfilename="$script_dir/log/$shfilename.log"
         current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
-        echo "[$current_datetime]: mole start remote --verbose --source 103.103.245.177:3004 --destination 127.0.0.1:3004 --server root@103.103.245.177 --key /root/.ssh/id_ed25519 -R 0 is start running ..." >>"$logfilename"
-        mole start remote --verbose --source 103.103.245.177:3004 --destination 127.0.0.1:3004 --server root@103.103.245.177 --key /root/.ssh/id_ed25519 -R 0
+        echo "[$current_datetime]: mole start remote --verbose --source 103.103.245.177:5000 --destination 127.0.0.1:5000 --server root@103.103.245.177 --key /root/.ssh/id_ed25519 -R 0 is start running ..." >>"$logfilename"
+        mole start remote --verbose --source 103.103.245.177:5000 --destination 127.0.0.1:5000 --server root@103.103.245.177 --key /root/.ssh/id_ed25519 -R 0
     else
-        echo "mole start remote --verbose --source 103.103.245.177:3004 --destination 127.0.0.1:3004 --server root@103.103.245.177 --key /root/.ssh/id_ed25519 -R 0 is already running with PID: $(pgrep -f "mole start remote --verbose --source 103.103.245.177:3004")"
+        echo "mole start remote --verbose --source 103.103.245.177:5000 --destination 127.0.0.1:5000 --server root@103.103.245.177 --key /root/.ssh/id_ed25519 -R 0 is already running with PID: $(pgrep -f "mole start remote --verbose --source 103.103.245.177:5000")"
     fi
     sleep 10
 done
